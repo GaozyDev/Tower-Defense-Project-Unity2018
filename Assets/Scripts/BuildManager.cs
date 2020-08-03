9676caf2b372c42a26979e0bedffa62102baa44b
Gzy <670453367@qq.com>
2020-08-03 19:22:31 +0800

    14-检查是否足够可以创建炮塔

---
+++ Assets/Scripts/BuildManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class BuildManager : MonoBehaviour
+{
+    public TurretData laserTurretData;
+    public TurretData missileTurretData;
+    public TurretData standardTurretData;
+
+    private TurretData selectedTurretData;
+
+    public int money = 1000;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (!EventSystem.current.IsPointerOverGameObject())
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+                bool isCollider = Physics.Raycast(ray, out hit, 1000, LayerMask.GetMask("MapCube"));
+                if (isCollider)
+                {
+                    MapCube mapCube = hit.collider.GetComponent<MapCube>();
+                    if (mapCube.turretGo == null)
+                    {
+                        if (money > selectedTurretData.cost)
+                        {
+                            money -= selectedTurretData.cost;
+                            mapCube.BuildTurret(selectedTurretData.turretPrefab);
+                        }
+                        else
+                        {
+
+                        }
+                    }
+                    else
+                    {
+                        
+                    }
+                }
+            }
+        }
+    }
+
+    public void OnLaserSelected(bool isOn)
+    {
+        if (isOn)
+        {
+            selectedTurretData = laserTurretData;
+        }
+    }
+
+    public void OnMissileSelected(bool isOn)
+    {
+        if (isOn)
+        {
+            selectedTurretData = missileTurretData;
+        }
+    }
+
+    public void OnStandardSelected(bool isOn)
+    {
+        if (isOn)
+        {
+            selectedTurretData = standardTurretData;
+        }
+    }
+}
---
+++ Assets/Scripts/MapCube.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapCube : MonoBehaviour
+{
+    [HideInInspector]
+    public GameObject turretGo;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void BuildTurret(GameObject turretPrefab)
+    {
+
+    }
+}
